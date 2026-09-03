45a0115ac53016422934de1a5b33147efb40c118
Jordan Reyes <arturosaravia32@gmail.com>
2026-09-03 17:26:27 -0500

    agrega .idea a .gitignore

--- src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
+++ src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
@@ -1,6 +1,6 @@
-package pe.edu.tecsup.lab03.controllers;
+package pe.edu.tecsup.lab03.repositories;
 
-public class StudentController {
+public class StudentRepository {
     public void procesarDatos() {
         System.out.println("Controlador StudentController activo");
     }
